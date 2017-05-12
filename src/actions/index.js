cb1fbb46bd52c79586f1069a49b4b0c9c9b49974
SpaceForestChu <spaceforestchu@gmail.com>
2017-05-12 19:52:58 -0400

    Second commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,10 +2,25 @@ export const ADD_TO_CART = 'ADD_TO_CART';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
 
 
-export const addToCart = (item) => ({
-  type: ADD_TO_CART,
-  payload: item,
-});
+
+export const addToCart = (item) => {
+  return (dispatch, getState) => {
+
+    const alreadyExists = (cartItem) => {
+      return cartItem.id === item.id;
+    }
+    const reduxState = getState();
+    const exists = reduxState.shoppingCart.filter(alreadyExists);
+
+
+    if(exists.length === 0) {
+      dispatch({
+        type: ADD_TO_CART,
+        payload: item,
+      });
+    }  
+  }
+}
 
 export const removeFromCart = (id) => ({
   type: REMOVE_FROM_CART,
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import App from './components/App';
 import './index.css';
 
 import reducers from './reducers';
 
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const store = createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
